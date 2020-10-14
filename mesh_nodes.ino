af91efc1421d5804de52d9c938cc198a993b97b5
iwilder53 <71007725+iwilder53@users.noreply.github.com>
2020-10-14 14:54:57 +0530

    Add files via upload

--- mesh_nodes.ino
+++ mesh_nodes.ino
@@ -1,3 +1,5 @@
+#include <SD.h>
+
 //************************************************************
 // This is an exmample for the nodes in the mesh network. Write root node id in the line 32.
 // For deepsleep mode connect D0 pin with RST pin of ESP8266(nodes) after uploading the code.
@@ -5,10 +7,14 @@
 //************************************************************
 
 #include "painlessMesh.h"
+#include <SPI.h> 
+
 
 #define   MESH_PREFIX     "HetaDatain"                         // Mesh Prefix (SSID) should be same for all  nodes in Mesh Network
 #define   MESH_PASSWORD   "Test@Run_1"                         // Mesh Password should be same for all  nodes in Mesh Network
-#define   MESH_PORT       5555                                      // Mesh Port should be same for all  nodes in Mesh Network
+#define   MESH_PORT       5555                                      // Mesh Port should be same for all  nodes in Mesh Network 
+
+ 
 
 Scheduler userScheduler; // to control your personal task
 painlessMesh  mesh;
@@ -19,21 +25,37 @@ unsigned long period=0;
 void sendMessage() ; // Prototype so PlatformIO doesn't complain
 
 
-Task taskSendMessage( TASK_SECOND * 2 , TASK_FOREVER, &sendMessage );   // Set task second to send msg in a time interval (Here interval is 4 second)
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );   // Set task second to send msg in a time interval (Here interval is 4 second)
 
 
 // If you want to receive sensor readings from this node, write code in below function....
 
 void sendMessage() {
+
+  
   String msg = " NODE no.1  ";                                       // You can write node name/no. here so that you may easily recognize it        
-  msg += mesh.getNodeId();                                              // Adding Node id in the msg
- // msg += " Analog: " + String (analogRead(A0));                          // Adding  analog reading in the msg. You can also add other pin readings 
+ // msg += mesh.getNodeId();                                              // Adding Node id in the msg
+   msg += " Analog: " + String (analogRead(A0));                          // Adding  analog reading in the msg. You can also add other pin readings 
  // msg += " myFreeMemory: " + String(ESP.getFreeHeap());                 // Adding free memory of Nodemcu in the msg
 // msg += "from end node1"; 
   uint32_t target = 314262534;                                         // Target is Node id of the node where you want to send sms (Here, write node id of mqtt bridge (Root Node))
-  mesh.sendSingle(target, msg );                                        // Send msg to single node. To broadcast msg (mesh.sendBroadcast(msg))
+  mesh.sendSingle(target, msg );                                        // Send msg to single node. To broadcast msg (mesh.sendBroadcast(msg)) 
   Serial.println(msg);
  //Serial.println("WiFi signal: " + String(WiFi.RSSI()) + " db");       // Prints wi-fi signal strength in db
+
+File dataFile = SD.open("offlinelog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(msg);
+    dataFile.close();
+    // print to the serial port too:
+    //Serial.println(msg);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
 }
 
 
@@ -58,9 +80,17 @@ void nodeTimeAdjustedCallback(int32_t offset) {
 
 void setup() {
   Serial.begin(115200);
+   
+    //CHECK FOR SDCARD
+    if (!SD.begin(D8)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
 
-//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes( ERROR | DEBUG );  // set before init() so that you can see startup messages
+// mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+ // mesh.setDebugMsgTypes( ERROR | DEBUG );  // set before init() so that you can see startup messages
 
   mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
   //mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6 );
@@ -76,8 +106,7 @@ void setup() {
   userScheduler.addTask( taskSendMessage );
   taskSendMessage.enable();
   pinMode(A0, INPUT);                                                    // Define A0 pin as INPUT
-                                        
-  
+ 
   
 }
 
@@ -88,9 +117,14 @@ period=millis()/1000;                                                    // Func
  
   mesh.update();
 
-              digitalWrite(LED_BUILTIN,LOW);                              // LED will be ON                                
+           //digitalWrite(LED_BUILTIN,LOW);                              // LED will be ON when node in not deepSleep mode                               
   
- 
+ /* if (period>60)                                                         // When period will be > 60 seconds, deep sleep mode will be active
+  {
+     mesh.stop();
+ ESP.deepSleep(300e6);                                                    // deepSleep mode will be active for 300*10^6 microseconds, i.e. for 300 seconds                                                   
+ digitalWrite(LED_BUILTIN,HIGH); 
+  }*/
 }
 void delayReceivedCallback(uint32_t from, int32_t delay) {
   Serial.printf("Delay to node %u is %d us\n", from, delay);
