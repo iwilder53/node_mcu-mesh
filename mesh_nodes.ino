29c265dcbe65a6b52945ed15d7e4486269fffc77
iwilder53 <71007725+iwilder53@users.noreply.github.com>
2020-10-14 18:21:57 +0530

    write to SD Card in case of wifi drop

--- mesh_nodes.ino
+++ mesh_nodes.ino
@@ -1,10 +1,4 @@
-#include <SD.h>
-
-//************************************************************
-// This is an exmample for the nodes in the mesh network. Write root node id in the line 32.
-// For deepsleep mode connect D0 pin with RST pin of ESP8266(nodes) after uploading the code.
-// Remove connection between D0 and RST pin while uploading code otherwise code will not be uploaded.
-//************************************************************
+#include <SD.h> 
 
 #include "painlessMesh.h"
 #include <SPI.h> 
@@ -23,43 +17,64 @@ unsigned long period=0;
 
 // User stub
 void sendMessage() ; // Prototype so PlatformIO doesn't complain
+void writeToCard() ;
+void loggedData();
 
 
-Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );   // Set task second to send msg in a time interval (Here interval is 4 second)
+Task taskSendMessage( TASK_SECOND * 4 , TASK_FOREVER, &sendMessage );   // Set task second to send msg in a time interval (Here interval is 4 second)
 
 
 // If you want to receive sensor readings from this node, write code in below function....
 
 void sendMessage() {
-
-  
+ 
   String msg = " NODE no.1  ";                                       // You can write node name/no. here so that you may easily recognize it        
  // msg += mesh.getNodeId();                                              // Adding Node id in the msg
    msg += " Analog: " + String (analogRead(A0));                          // Adding  analog reading in the msg. You can also add other pin readings 
  // msg += " myFreeMemory: " + String(ESP.getFreeHeap());                 // Adding free memory of Nodemcu in the msg
 // msg += "from end node1"; 
-  uint32_t target = 314262534;                                         // Target is Node id of the node where you want to send sms (Here, write node id of mqtt bridge (Root Node))
-  mesh.sendSingle(target, msg );                                        // Send msg to single node. To broadcast msg (mesh.sendBroadcast(msg)) 
+  uint32_t target = 314262534; 
+   mesh.sendSingle(target, msg );                                        // Send msg to single node. To broadcast msg (mesh.sendBroadcast(msg)) 
   Serial.println(msg);
- //Serial.println("WiFi signal: " + String(WiFi.RSSI()) + " db");       // Prints wi-fi signal strength in db
+  Serial.println("WiFi signal: " + String(WiFi.RSSI()) + " db");
 
-File dataFile = SD.open("offlinelog.txt", FILE_WRITE);
 
-  // if the file is available, write to it:
+        
+
+  } 
+
+
+ Task taskWriteToCard( TASK_SECOND * 4 , TASK_FOREVER, &writeToCard );
+
+ 
+ void writeToCard()
+ { 
+
+  String msg = " NODE no.1  "; 
+   msg += " Analog: " + String (analogRead(A0)); 
+   
+    File dataFile = SD.open("offlinelog.txt", FILE_WRITE);
+// if the file is available, write to it:
   if (dataFile) {
     dataFile.println(msg);
     dataFile.close();
     // print to the serial port too:
-    //Serial.println(msg);
-  }
+    Serial.println("to SD Card"); 
+    Serial.println(msg);
+    Serial.println("WiFi signal: " + String(WiFi.RSSI()) + " db");}
   // if the file isn't open, pop up an error:
   else {
-    Serial.println("error opening datalog.txt");
+    Serial.println("error opening datalog.txt"); 
+    }
+  } 
+
+  
+   Task taskLoggedData(TASK_SECOND * 4 , TASK_FOREVER, &loggedData );
+
+void loggedData(){
+  
   }
-}
 
-
-// Msg recived by node. If you want to perform any task by receiving msg, write code in the below function....
 
 void receivedCallback( uint32_t from, String &msg ) {
  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
@@ -80,21 +95,23 @@ void nodeTimeAdjustedCallback(int32_t offset) {
 
 void setup() {
   Serial.begin(115200);
-   
+    
     //CHECK FOR SDCARD
     if (!SD.begin(D8)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1);
+    
+   Serial.println("Card failed, or not present");
+     // don't do anything more:
+     while (1);
   }
-  Serial.println("card initialized.");
+  Serial.println("card initialized."); 
 
 // mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
- // mesh.setDebugMsgTypes( ERROR | DEBUG );  // set before init() so that you can see startup messages
+  mesh.setDebugMsgTypes( ERROR | DEBUG | CONNECTION);  // set before init() so that you can see startup messages
 
   mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
   //mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6 );
   mesh.setContainsRoot(true);
+  
   
   
   mesh.onReceive(&receivedCallback);
@@ -104,7 +121,10 @@ void setup() {
   mesh.onNodeDelayReceived(&delayReceivedCallback);
 
   userScheduler.addTask( taskSendMessage );
+  userScheduler.addTask(taskWriteToCard);
+  userScheduler.addTask(taskLoggedData);
   taskSendMessage.enable();
+  
   pinMode(A0, INPUT);                                                    // Define A0 pin as INPUT
  
   
@@ -116,8 +136,15 @@ void loop() {
 period=millis()/1000;                                                    // Function "mllis()" gives time in milliseconds. Here "period" will store time in seconds
  
   mesh.update();
+ //Serial.println("WiFi signal: " + String(WiFi.RSSI()) + " db");       // Prints wi-fi signal strength in db
 
-           //digitalWrite(LED_BUILTIN,LOW);                              // LED will be ON when node in not deepSleep mode                               
+
+  
+  if( (WiFi.RSSI()) == 31 ){
+    taskWriteToCard.enable();
+     digitalWrite(LED_BUILTIN,LOW);                              // LED will be ON when node is writibng to sd card                                
+    } 
+
   
  /* if (period>60)                                                         // When period will be > 60 seconds, deep sleep mode will be active
   {
@@ -129,3 +156,4 @@ period=millis()/1000;                                                    // Func
 void delayReceivedCallback(uint32_t from, int32_t delay) {
   Serial.printf("Delay to node %u is %d us\n", from, delay);
 }
+
